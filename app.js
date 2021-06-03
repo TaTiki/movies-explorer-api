32f69af4264cc7c9e0ec26eee7b58af0c785f8cc
tatiana_kirsanova <alisia7788@mail.ru>
2021-06-03 21:26:11 +0300

    редактирование для русских букв

--- app.js
+++ app.js
@@ -27,8 +27,7 @@ app.post('/signup', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
-    name: Joi.string().required().min(2).max(30)
-      .alphanum(),
+    name: Joi.string().required().min(2).max(30),
   }),
 }), createUser);
 
