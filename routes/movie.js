0fe8e733157a050174671b991c7ecfef1e7d0abd
tatiana_kirsanova <alisia7788@mail.ru>
2021-06-17 07:21:03 +0300

    correction

--- routes/movie.js
+++ routes/movie.js
@@ -26,7 +26,7 @@ router.post('/', celebrate({
 
 router.delete('/:movieId', celebrate({
   params: Joi.object().keys({
-    movieId: Joi.number().required(),
+    movieId: Joi.string().required().length(24).hex(),
   }),
 }), deleteMovie);
 
